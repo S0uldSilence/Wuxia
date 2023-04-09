c75534feafea5cb08c397dc8b55ac7d62caef177
S0uldSilence <S0uld.Silence@gmail.com>
2023-04-09 21:58:56 +0200

    Improved Essence System.

--- src/main/java/com/github/s0uldsilence/wuxia/block/essence/EssenceGeneratorBE.java
+++ src/main/java/com/github/s0uldsilence/wuxia/block/essence/EssenceGeneratorBE.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,14 +22,30 @@ public class EssenceGeneratorBE extends EssenceBaseGeneratorBE {
         if (level.isClientSide()) {
             return;
         }
-        pEntity.receiveEssence(10, false);
-        if (pEntity.getPosition(0) != null) {
-            BlockPos linkedBlockPos = pEntity.getPosition(0);
-            BlockEntity linkedBlockEntity = level.getBlockEntity(linkedBlockPos);
-            if (linkedBlockEntity instanceof EssenceBaseStorageBE storage) {
-                pEntity.extractEssence(10, false);
-                storage.receiveEssence(10, false);
+
+        // Generate essence
+        int generated = pEntity.receiveEssence(pEntity.getMaxReceive(), false);
+
+        // Transfer essence to connected storage blocks
+        List<IEssenceStorage> storages = getConnectedStorages(level, pos, pEntity);
+        int remaining = generated;
+        for (IEssenceStorage storage : storages) {
+            int transferred = storage.receiveEssence(pEntity.getMaxExtract(), false);
+            remaining -= transferred;
+            if (remaining <= 0) {
+                break;
             }
         }
+        pEntity.extractEssence(generated - remaining, false);
+    }
+    private static List<IEssenceStorage> getConnectedStorages(Level level, BlockPos pos, EssenceBaseGeneratorBE generator) {
+        BlockPos[] positionsArray = generator.getPositions();
+        List<BlockPos> positions = new ArrayList<>(Arrays.asList(positionsArray));
+        positions.add(pos); // Add own position to check for adjacent storage blocks
+        return positions.stream()
+                .map(level::getBlockEntity)
+                .filter(be -> be instanceof IEssenceStorage)
+                .map(be -> (IEssenceStorage) be)
+                .collect(Collectors.toList());
     }
 }
